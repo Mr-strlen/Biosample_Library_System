a8543ef7b4e583355c151d28fd59d949bbb849f2
FrankCui <FrankCui713@163.com>
2020-07-15 20:47:26 +0800

    Jump to the page

---
+++ src/com/bsb/controller/CheckController.java
@@ -0,0 +1,20 @@
+package com.bsb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/check")
+
+public class CheckController {
+	@RequestMapping("/creat")
+    public ModelAndView getCreat(){
+        ModelAndView mv = new ModelAndView("CheckCreat");
+        return mv;
+	}
+}
