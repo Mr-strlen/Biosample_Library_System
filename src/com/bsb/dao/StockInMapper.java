52ebc6b728792be7991602039ed0f42d253f4f3a
dongyubo <daniel_y_dong@163.com>
2020-07-17 15:55:43 +0800

    Create DAO - StockInMapper.java

---
+++ src/com/bsb/dao/StockInMapper.java
@@ -0,0 +1,15 @@
+package com.bsb.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.bsb.model.StockInAppdetail;
+import com.bsb.model.StockInAppsum;
+import com.bsb.model.StockInRecord;
+
+@Mapper
+public interface StockInMapper {
+	
+}
--- src/com/bsb/service/impl/StockInServiceImpl.java
+++ src/com/bsb/service/impl/StockInServiceImpl.java
@@ -6,3 +6,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bsb.dao.StockInMapper;
+
+import com.bsb.model.StockInAppsum;
+import com.bsb.model.StockInRecord;
+import com.bsb.model.StockInAppdetail;
+
+import com.bsb.service.StockInService;
+
+@Service("StockInService")
+public class StockInServiceImpl implements StockInService{
+
+}
