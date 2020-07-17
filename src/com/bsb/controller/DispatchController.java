e6c68187a3950c3b6964a350c66b2aec6c984565
warku123 <warku123@users.noreply.github.com>
2020-07-17 18:06:00 +0800

    导入了查询组件

--- src/com/bsb/controller/DispatchController.java
+++ src/com/bsb/controller/DispatchController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.bsb.service.DispatchService;
+import com.bsb.service.SampleInfoService;
 
 @Controller
 @RequestMapping("/dispatch")
@@ -17,6 +18,8 @@ public class DispatchController {
 	
 	@Autowired
 	private DispatchService dispatchService;
+	@Autowired
+	private SampleInfoService sampleinfoService;
 	
 	//出库申请
 	@RequestMapping("/apply")
@@ -73,6 +76,121 @@ public class DispatchController {
     public ModelAndView setCheckDetail(String type){
         ModelAndView mv = new ModelAndView("DispatchAppCheckDetail");
         mv.addObject("app_checkdetail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	// 申请审核提交
+	@RequestMapping("/app_checksubmit")
+	public ModelAndView submitcheck(String order, String type, String result,  String reason_pos, String auditor) {
+		//System.out.println(order+" "+type+" "+result+" "+reason_pos);
+		String[] orders = order.split(",");
+		order = orders[0];
+		String[] types = type.split(",");
+		String[] results = result.split(",");
+		String[] reason_poss = reason_pos.split(",");
+		boolean flag = true;
+		for(int i = 0; i < types.length; i++) {
+			dispatchService.appCheck(order, types[i], results[i], reason_poss[i]);
+			if(results[i].equals("未通过")) {
+				flag = false;
+			}
+		}
+		if(flag) {
+			dispatchService.appCheckFinal(order, "已通过", auditor);
+			
+			// 向Record新增数据
+			int num = dispatchService.getRecordNum();
+			num = num + 1;
+			String recordorder = "D" + String.valueOf(num);
+			
+			
+			String application_order = order;
+			order = recordorder;
+			dispatchService.insertRecord(order, application_order, "待完成");
+		}
+		else {
+			dispatchService.appCheckFinal(order, "未通过", auditor);
+			
+		}	
+		return(new ModelAndView("redirect:app_checksum"));
+	}
+	
+	// 待出库订单
+	@RequestMapping("/record_opsum")
+	public ModelAndView getRecordOpSum(){
+        ModelAndView mv = new ModelAndView("DispatchRecordOpSum");
+        mv.addObject("record_opsum",dispatchService.findRecordByState("待完成"));
+        return mv;
+    }
+	
+	// 出库操作详情
+	@RequestMapping("/record_opdetail")
+    public ModelAndView setOpDetail(String type){
+        ModelAndView mv = new ModelAndView("DispatchRecordOpDetail");
+        mv.addObject("record_opdetail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	
+	// 出库提交
+	@RequestMapping("/record_checksubmit")
+	public ModelAndView recordSubmitcheck(String application_order, String manager) {
+		// 更新记录表
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time = sFormat.format(new Date());
+		
+		String order = dispatchService.getAppOrderByOrder(application_order);
+		//System.out.println(order+" "+ time+" "+manager+" "+application_order);
+		
+		dispatchService.updateRecord(order, time, manager, "已完成");
+		
+		List<String> ls = dispatchService.getPosByOrder(application_order);
+		for(String pos : ls) {
+			  
+			String[] reason_poss = pos.split(",");
+			for(int i = 0; i < reason_poss.length; i++) {
+				// 总表中删除
+				String position = reason_poss[i];
+				System.out.println(position);
+				sampleinfoService.DeletRecordByPosition(position);
+			}
+		}
+		
+		return(new ModelAndView("redirect:record_opsum"));
+	}
+	
+	// 订单（申请）详情
+	@RequestMapping("/record_detail")
+	public ModelAndView getRecordDetail(String id, String type){
+        ModelAndView mv = new ModelAndView("DispatchRecordDetail");
+        mv.addObject("id", id);
+        mv.addObject("record_detail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	
+	// 	出库订单查询
+	@RequestMapping("/record_select")
+	public ModelAndView getSelectRecord(String type, String word){
+        ModelAndView mv = new ModelAndView("DispatchRecordSelect");
+        if(word == "") {
+        	mv.addObject("record_select",dispatchService.findAllRecord());
+        }
+        else {
+        	if(type == null) {
+				mv.addObject("record_select", dispatchService.findAllRecord());
+				return mv;
+			}
+			if(Integer.valueOf(type) == 1) {
+				mv.addObject("record_select", dispatchService.findRecordByOrder(word));
+			}
+			else if(Integer.valueOf(type) == 2) {
+				mv.addObject("record_select", dispatchService.findRecordByTime(word));
+			}
+			else if(Integer.valueOf(type) == 3) {
+				mv.addObject("record_select", dispatchService.findRecordByManager(word));
+			}
+			else if(Integer.valueOf(type) == 4) {
+				mv.addObject("record_select", dispatchService.findRecordByState(word));
+			}
+        }
         return mv;
     }
 }
---
+++ src/com/bsb/service/SampleInfoService.java
@@ -0,0 +1,30 @@
+package com.bsb.service;
+
+import java.util.List;
+
+import com.bsb.model.SampleInfoTable;
+import com.bsb.model.SampleStatisticsTable;
+
+public interface SampleInfoService {
+	public List<SampleInfoTable> getAllInfoTables();
+	public List<SampleInfoTable> NameQueryTables(String name);
+	public List<SampleInfoTable> TypeQueryTables(String type);
+	public List<SampleInfoTable> AuditorQueryTables(String auditor);
+	public List<SampleInfoTable> SourceQueryTables(String source);
+	public List<SampleInfoTable> TimeQueryTables(String time);
+	public List<SampleInfoTable> PositionQueryTables(String position);
+	public String GetUnusedPosition(String type);
+	public String GetUnusedNumByType(String type);
+	public String GetNumByName(String name);
+	public String GetNumBySource(String source);
+	public String GetRestNum(String type);
+	
+	public void DeletRecordByName(String name);
+	public void DeletRecordByPosition(String position);
+	public void DeletRecordById(String id);
+	
+	public List<SampleStatisticsTable> GroupByName();
+	public List<SampleStatisticsTable> GroupByType();
+	public List<SampleStatisticsTable> GroupByAuditor();
+	public List<SampleStatisticsTable> GroupBySource();
+}
