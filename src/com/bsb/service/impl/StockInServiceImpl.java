fb35575294ff1dc5807e3649fb6221098fba1e6f
dongyubo <daniel_y_dong@163.com>
2020-07-17 23:39:50 +0800

    Update StockInServiceImpl.java

--- src/com/bsb/service/impl/StockInServiceImpl.java
+++ src/com/bsb/service/impl/StockInServiceImpl.java
@@ -18,7 +18,7 @@ public class StockInServiceImpl implements StockInService{
 	@Autowired
 	private StockInMapper StockInMapper;
 	
-	//1.入库申请表汇总
+	/*1.入库申请表汇总*/
 	//展示所有申请表
 	@Override
 	public List<StockInAppsum> ShowAppsum(int state) {
@@ -26,8 +26,30 @@ public class StockInServiceImpl implements StockInService{
 	}
 	
 	//获取下一个申请表单号
-		@Override
-		public int GetNextAppId() {
-			return StockInMapper.GetNextAppId();
-		}
+	@Override
+	public int GetNextAppId() {
+		return StockInMapper.GetNextAppId();
+	}
+	
+	/*2.入库申请表详细*/
+	//显示申请表信息
+	@Override
+	public List<StockInAppdetail> ShowAppdetail(int id) {
+		return StockInMapper.ShowAppdetail(id);
+	}
+	
+	//插入一次申请记录
+	@Override
+	public void Appsubmit(int id, String applicant, String name,String size,String condition) {
+		StockInMapper.Appsubmit(id, applicant, name, size, condition);
+	}
+	
+	//申请表审核
+	@Override
+	public void Appcheck(int id, String name, String result, String reason) {
+		 StockInMapper.Appcheck(id, name, result, reason);
+	}
+	
+	
+	
 }
