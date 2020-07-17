bc0a30a3923592f47870f9343cd64d335693b9a2
dongyubo <daniel_y_dong@163.com>
2020-07-18 00:41:22 +0800

    fix CAPS error in controller
    fix CAPS error in controller

--- src/com/bsb/controller/StockInController.java
+++ src/com/bsb/controller/StockInController.java
@@ -9,7 +9,7 @@ import com.bsb.service.StockInService;
 //import com.bsb.service.SampleInfoService;
 
 @Controller
-@RequestMapping("/StockIn")
+@RequestMapping("/stockin")
 public class StockInController {
 	@Autowired
     private StockInService StockInService;
@@ -17,47 +17,47 @@ public class StockInController {
 //	private SampleInfoService sampleinfoService;
 	
 	//审核申请界面
-	@RequestMapping("/StockIn_checking")
+	@RequestMapping("/stockin_checking")
     public ModelAndView showappchecking(){
         ModelAndView mv = new ModelAndView("StockInAppChecking");
-        mv.addObject("StockIn_checking",StockInService.ShowAppsum(Integer.valueOf("0")));
+        mv.addObject("stockin_checking",StockInService.ShowAppsum(Integer.valueOf("0")));
         return mv;
     }
 	
 	//已审核申请界面
-	@RequestMapping("/StockIn_checked")
+	@RequestMapping("/stockin_checked")
     public ModelAndView showappchecked(){
         ModelAndView mv = new ModelAndView("StockInAppChecked");
-        mv.addObject("StockIn_checked",StockInService.ShowAppsum(Integer.valueOf("1")));
+        mv.addObject("stockin_checked",StockInService.ShowAppsum(Integer.valueOf("1")));
         return mv;
     }
 	
 	//申请详情
-	@RequestMapping("/StockIn_appdetail")
+	@RequestMapping("/stockin_appdetail")
     public ModelAndView showappdetail(String id){
         ModelAndView mv = new ModelAndView("StockInAppdetail");
-        mv.addObject("StockIn_appdetail",StockInService.ShowAppdetail(Integer.valueOf(id)));
+        mv.addObject("stockin_appdetail",StockInService.ShowAppdetail(Integer.valueOf(id)));
         return mv;
     }
 	
 	//入库记录
-	@RequestMapping("/StockIn_record")
+	@RequestMapping("/stockin_record")
     public ModelAndView showrecord(){
         ModelAndView mv = new ModelAndView("StockInRecord");
-        mv.addObject("StockIn_record",StockInService.ShowRecord());
+        mv.addObject("stockin_record",StockInService.ShowRecord());
         return mv;
     }
 	
 	//库存详细信息
-	@RequestMapping("/StockIn_check")
+	@RequestMapping("/stockin_check")
     public ModelAndView showcheck(String id){
         ModelAndView mv = new ModelAndView("StockInCheck");
-        mv.addObject("StockIn_appdetail",StockInService.ShowAppdetail(Integer.valueOf(id)));
+        mv.addObject("stockin_appdetail",StockInService.ShowAppdetail(Integer.valueOf(id)));
         String[] storage = {"1","2","3"};
 //        storage[0]=sampleinfoService.GetUnusedNumByType("1");
 //        storage[1]=sampleinfoService.GetUnusedNumByType("2");
 //        storage[2]=sampleinfoService.GetUnusedNumByType("3");
-        mv.addObject("StockIn_storagedetail",storage);
+        mv.addObject("stockin_storagedetail",storage);
         for(String i:storage) {
         	System.out.println(i);
         }
@@ -65,7 +65,7 @@ public class StockInController {
     }
 	
 	//进行审核
-	@RequestMapping("/StockIn_checksubmit")
+	@RequestMapping("/stockin_checksubmit")
 	public ModelAndView submitcheck(int id, String name, String reason,  String result) {
 		System.out.println(id+" "+name+" "+result+" "+reason);
 		String[] names = name.split(",");
@@ -75,41 +75,41 @@ public class StockInController {
 			
 			StockInService.Appcheck(Integer.valueOf(id), names[i], results[i], reasons[i]);
 		}
-		return(new ModelAndView("redirect:StockIn_checking"));
+		return(new ModelAndView("redirect:stockin_checking"));
 	}
 	
 	//申请记录提交
-	@RequestMapping("/StockIn_recordsubmit")
+	@RequestMapping("/stockin_recordsubmit")
 	public ModelAndView submitrecord( int application_id,  String sample_id, String sample_name, String type) {
 //		String position_id = sampleinfoService.GetUnusedPosition(type);
 		int id = StockInService.GetNextRecordId();
 //		StockInService.Recordsubmit(id, application_id, sample_id, sample_name, position_id);
 		
-		return(new ModelAndView("redirect:StockIn_record"));
+		return(new ModelAndView("redirect:stockin_record"));
 	}
 	
 	//申请记录查询
-	@RequestMapping("/StockIn_findrecord")
+	@RequestMapping("/stockin_findrecord")
     public ModelAndView findrecord(String sample_id){
 		System.out.println(sample_id);
 		ModelAndView mv = new ModelAndView("StockInRecord");
-        mv.addObject("StockIn_record",StockInService.ShowRecordbySample_id(sample_id));
+        mv.addObject("stockin_record",StockInService.ShowRecordbySample_id(sample_id));
         return mv;
     }
 	
 	//入库申请填写
-	@RequestMapping("/StockIn_app")
+	@RequestMapping("/stockin_app")
     public ModelAndView app(){
 		ModelAndView mv = new ModelAndView("StockInAppsubmit");
-        mv.addObject("StockIn_nextappid",StockInService.GetNextAppId());        
+        mv.addObject("stockin_nextappid",StockInService.GetNextAppId());        
         return mv;
     }
 	
 	//入库申请提交
-	@RequestMapping("/StockIn_appsubmit")
+	@RequestMapping("/stockin_appsubmit")
     public ModelAndView submitapp(int id, String applicant, String name,String size,String condition){				
         StockInService.Appsubmit(id, applicant, name, size, condition);
-        return(new ModelAndView("redirect:StockIn_app"));
+        return(new ModelAndView("redirect:stockin_app"));
     }
 	
 }
