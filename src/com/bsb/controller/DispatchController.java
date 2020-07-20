326db0c8a437b4b64f9e636ea15f95b7c43b4b99
seanlyp <59121206+seanlyp@users.noreply.github.com>
2020-07-20 23:11:46 +0800

    修改1

---
+++ src/com/bsb/controller/DispatchController.java
@@ -0,0 +1,248 @@
+package com.bsb.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bsb.service.DispatchService;
+import com.bsb.service.SampleInfoService;
+
+@Controller
+@RequestMapping("/dispatch")
+public class DispatchController {
+	@Autowired
+    private DispatchService dispatchService;
+	@Autowired
+	private SampleInfoService sampleinfoService;
+	
+	@RequestMapping("/apply")
+	public ModelAndView getApply(){
+        ModelAndView mv = new ModelAndView("DispatchApply");
+        return mv;
+    }
+	
+	/*// 申请单个
+	@RequestMapping("/apply_deliverdetail")
+	public ModelAndView getApplyDe(String type, String snum, String condition){
+        // ModelAndView mv = new ModelAndView("DispatchApplySum");
+        int anum = dispatchService.getApplyNum();
+		anum = anum + 1;
+		String order = "DA" + String.valueOf(anum);
+		System.out.println("*******" + type + snum + condition);
+		int num = Integer.valueOf(snum);
+		
+		dispatchService.insertApplyDe(order, type, num, condition, "待审核");       
+		return(new ModelAndView("redirect:apply"));
+    }*/
+	
+	// 申请完成
+	@RequestMapping("/apply_deliversum")
+	public ModelAndView getApplySum(String applicant, String type, String snum, String condition){
+		ModelAndView mv = new ModelAndView("DispatchApply");
+		mv.addObject("applicant", applicant);
+		mv.addObject("type", type);
+		mv.addObject("snum", snum);
+		mv.addObject("condition", condition);
+		
+		String[] types = type.split(",");
+		String[] snums = snum.split(",");
+		String[] conditions = condition.split(",");
+
+        int anum = dispatchService.getApplyNum();
+		anum = anum + 1;
+		String order = "DA" + String.valueOf(anum);
+		
+		String result = "待审核";
+		
+		for(int i = 0; i < types.length; i++) {
+			int num = Integer.valueOf(snums[i]);
+			type = types[i];
+			condition = conditions[i];
+			dispatchService.insertApplyDe(order, type, num, condition, result); 
+		}
+		
+		
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time = sFormat.format(new Date());
+		
+		dispatchService.insertApplySum(order, applicant, time, result);
+        
+        // 返回界面
+        return(new ModelAndView("redirect:app_checksum"));
+    }
+	
+	// 待审核申请
+	@RequestMapping("/app_checksum")
+	public ModelAndView getCheckApp(){
+        ModelAndView mv = new ModelAndView("DispatchAppCheckSum");
+        mv.addObject("app_checksum",dispatchService.findAppByResult("待审核"));
+        return mv;
+    }
+	
+	// 申请查询
+	@RequestMapping("/app_select")
+	public ModelAndView getSelectApp(String type, String word){
+        ModelAndView mv = new ModelAndView("DispatchAppSelect");
+        if(word == "") {
+        	mv.addObject("app_select",dispatchService.findAllApp());
+        }
+        else {
+        	if(type == null) {
+				mv.addObject("app_select", dispatchService.findAllApp());
+				return mv;
+			}
+			if(Integer.valueOf(type) == 1) {
+				mv.addObject("app_select", dispatchService.findAppByOrder(word));
+			}
+			else if(Integer.valueOf(type) == 2) {
+				mv.addObject("app_select", dispatchService.findAppByApplicant(word));
+			}
+			else if(Integer.valueOf(type) == 3) {
+				mv.addObject("app_select", dispatchService.findAppByTime(word));
+			}
+			else if(Integer.valueOf(type) == 4) {
+				mv.addObject("app_select", dispatchService.findAppByResult(word));
+			}
+			else if(Integer.valueOf(type) == 5) {
+				mv.addObject("app_select", dispatchService.findAppByAuditor(word));
+			}
+        }
+        return mv;
+    }
+	
+	// 申请详情
+	@RequestMapping("/app_detail")
+	public ModelAndView getAppDetail(String type){
+        ModelAndView mv = new ModelAndView("DispatchAppDetail");
+        mv.addObject("app_detail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	
+	// 审核申请
+	@RequestMapping("/app_checkdetail")
+    public ModelAndView setCheckDetail(String type){
+        ModelAndView mv = new ModelAndView("DispatchAppCheckDetail");
+        mv.addObject("app_checkdetail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	// 申请审核提交
+	@RequestMapping("/app_checksubmit")
+	public ModelAndView submitcheck(String order, String type, String result,  String reason_pos, String auditor) {
+		//System.out.println(order+" "+type+" "+result+" "+reason_pos);
+		String[] orders = order.split(",");
+		order = orders[0];
+		String[] types = type.split(",");
+		String[] results = result.split(",");
+		String[] reason_poss = reason_pos.split(",");
+		boolean flag = true;
+		for(int i = 0; i < types.length; i++) {
+			dispatchService.appCheck(order, types[i], results[i], reason_poss[i]);
+			if(results[i].equals("未通过")) {
+				flag = false;
+			}
+		}
+		if(flag) {
+			dispatchService.appCheckFinal(order, "已通过", auditor);
+			
+			// 向Record新增数据
+			int num = dispatchService.getRecordNum();
+			num = num + 1;
+			String recordorder = "D" + String.valueOf(num);
+			
+			
+			String application_order = order;
+			order = recordorder;
+			dispatchService.insertRecord(order, application_order, "待完成");
+		}
+		else {
+			dispatchService.appCheckFinal(order, "未通过", auditor);
+			
+		}	
+		return(new ModelAndView("redirect:app_checksum"));
+	}
+	
+	// 待出库订单
+	@RequestMapping("/record_opsum")
+	public ModelAndView getRecordOpSum(){
+        ModelAndView mv = new ModelAndView("DispatchRecordOpSum");
+        mv.addObject("record_opsum",dispatchService.findRecordByState("待完成"));
+        return mv;
+    }
+	
+	// 出库操作详情
+	@RequestMapping("/record_opdetail")
+    public ModelAndView setOpDetail(String type){
+        ModelAndView mv = new ModelAndView("DispatchRecordOpDetail");
+        mv.addObject("record_opdetail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	
+	// 出库提交
+	@RequestMapping("/record_checksubmit")
+	public ModelAndView recordSubmitcheck(String application_order, String manager) {
+		// 更新记录表
+		SimpleDateFormat sFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String time = sFormat.format(new Date());
+		
+		String order = dispatchService.getAppOrderByOrder(application_order);
+		//System.out.println(order+" "+ time+" "+manager+" "+application_order);
+		
+		dispatchService.updateRecord(order, time, manager, "已完成");
+		
+		List<String> ls = dispatchService.getPosByOrder(application_order);
+		for(String pos : ls) {
+			  
+			String[] reason_poss = pos.split(",");
+			for(int i = 0; i < reason_poss.length; i++) {
+				// 总表中删除
+				String position = reason_poss[i];
+				System.out.println(position);
+				sampleinfoService.DeletRecordByPosition(position);
+			}
+		}
+		
+		return(new ModelAndView("redirect:record_opsum"));
+	}
+	
+	// 订单（申请）详情
+	@RequestMapping("/record_detail")
+	public ModelAndView getRecordDetail(String id, String type){
+        ModelAndView mv = new ModelAndView("DispatchRecordDetail");
+        mv.addObject("id", id);
+        mv.addObject("record_detail",dispatchService.findAppDetailByOrder(type));
+        return mv;
+    }
+	
+	// 	出库订单查询
+	@RequestMapping("/record_select")
+	public ModelAndView getSelectRecord(String type, String word){
+        ModelAndView mv = new ModelAndView("DispatchRecordSelect");
+        if(word == "") {
+        	mv.addObject("record_select",dispatchService.findAllRecord());
+        }
+        else {
+        	if(type == null) {
+				mv.addObject("record_select", dispatchService.findAllRecord());
+				return mv;
+			}
+			if(Integer.valueOf(type) == 1) {
+				mv.addObject("record_select", dispatchService.findRecordByOrder(word));
+			}
+			else if(Integer.valueOf(type) == 2) {
+				mv.addObject("record_select", dispatchService.findRecordByTime(word));
+			}
+			else if(Integer.valueOf(type) == 3) {
+				mv.addObject("record_select", dispatchService.findRecordByManager(word));
+			}
+			else if(Integer.valueOf(type) == 4) {
+				mv.addObject("record_select", dispatchService.findRecordByState(word));
+			}
+        }
+        return mv;
+    }
+}
---
+++ src/com/bsb/controller/WarehouseController.java
@@ -0,0 +1,106 @@
+package com.bsb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bsb.service.WarehouseService;
+import com.bsb.service.SampleInfoService;;
+
+@Controller
+@RequestMapping("/warehouse")
+public class WarehouseController {
+	@Autowired
+    private WarehouseService warehouseService;
+	@Autowired
+	private SampleInfoService sampleinfoService;
+	
+	@RequestMapping("/warehouse_checking")
+    public ModelAndView showappchecking(){
+        ModelAndView mv = new ModelAndView("WarehouseAppChecking");
+        mv.addObject("warehouse_checking",warehouseService.ShowAppsum(Integer.valueOf("0")));
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_checked")
+    public ModelAndView showappchecked(){
+        ModelAndView mv = new ModelAndView("WarehouseAppChecked");
+        mv.addObject("warehouse_checked",warehouseService.ShowAppsum(Integer.valueOf("1")));
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_appdetail")
+    public ModelAndView showappdetail(String id){
+        ModelAndView mv = new ModelAndView("WarehouseAppdetail");
+        mv.addObject("warehouse_appdetail",warehouseService.ShowAppdetail(Integer.valueOf(id)));
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_record")
+    public ModelAndView showrecord(){
+        ModelAndView mv = new ModelAndView("WarehouseRecord");
+        mv.addObject("warehouse_record",warehouseService.ShowRecord());
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_check")
+    public ModelAndView showcheck(String id){
+        ModelAndView mv = new ModelAndView("WarehouseCheck");
+        mv.addObject("warehouse_appdetail",warehouseService.ShowAppdetail(Integer.valueOf(id)));
+        String[] storage = {"1","2","3"};
+        storage[0]=sampleinfoService.GetUnusedNumByType("1");
+        storage[1]=sampleinfoService.GetUnusedNumByType("2");
+        storage[2]=sampleinfoService.GetUnusedNumByType("3");
+        mv.addObject("warehouse_storagedetail",storage);
+        for(String i:storage) {
+        	System.out.println(i);
+        }
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_checksubmit")
+	public ModelAndView submitcheck(int id, String name, String reason,  String result) {
+		System.out.println(id+" "+name+" "+result+" "+reason);
+		String[] names = name.split(",");
+		String[] results = result.split(",");
+		String[] reasons = reason.split(",");
+		for(int i = 0; i < names.length; i++) {
+			
+			warehouseService.Appcheck(Integer.valueOf(id), names[i], results[i], reasons[i]);
+		}
+		return(new ModelAndView("redirect:warehouse_checking"));
+	}
+	
+	@RequestMapping("/warehouse_recordsubmit")
+	public ModelAndView submitrecord( int application_id,  String sample_id, String sample_name, String type) {
+		String position_id = sampleinfoService.GetUnusedPosition(type);
+		int id = warehouseService.GetNextRecordId();
+		warehouseService.Recordsubmit(id, application_id, sample_id, sample_name, position_id);
+		
+		return(new ModelAndView("redirect:warehouse_record"));
+	}
+	
+	@RequestMapping("/warehouse_findrecord")
+    public ModelAndView findrecord(String sample_id){
+		System.out.println(sample_id);
+		ModelAndView mv = new ModelAndView("WarehouseRecord");
+        mv.addObject("warehouse_record",warehouseService.ShowRecordbySample_id(sample_id));
+        return mv;
+    }
+	
+	@RequestMapping("/warehouse_app")
+    public ModelAndView app(){		
+		ModelAndView mv = new ModelAndView("WarehouseAppsubmit");
+        mv.addObject("warehouse_nextappid",warehouseService.GetNextAppId());        
+        return mv;
+    }
+	
+	
+	@RequestMapping("/warehouse_appsubmit")
+    public ModelAndView submitapp(int id, String applicant, String name,String size,String condition){				
+        warehouseService.Appsubmit(id, applicant, name, size, condition);
+        return(new ModelAndView("redirect:warehouse_app"));
+    }
+	
+}
