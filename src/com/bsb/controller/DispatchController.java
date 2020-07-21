9240461dc8f262daa19ebf2312e9a6318d26fd38
warku123 <warku123@users.noreply.github.com>
2020-07-21 14:17:28 +0800

    Update DispatchController.java
    删除了一行无用的import

--- src/com/bsb/controller/DispatchController.java
+++ src/com/bsb/controller/DispatchController.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 //import java.util.List;
 
-import org.aspectj.weaver.ast.Var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
