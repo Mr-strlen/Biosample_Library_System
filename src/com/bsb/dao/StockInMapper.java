7979caa99afb9fdc9a0504a1e06766addc307511
dongyubo <daniel_y_dong@163.com>
2020-07-21 20:21:05 +0800

    fix GBK to utf-8 bug
    fix GBK to utf-8 bug

--- src/com/bsb/dao/StockInMapper.java
+++ src/com/bsb/dao/StockInMapper.java
@@ -11,24 +11,24 @@ import com.bsb.model.StockInRecord;
 
 @Mapper
 public interface StockInMapper {
-	//1.鍏ュ簱鐢宠琛ㄦ眹鎬�
-	public List<StockInAppsum> ShowAppsum(int state);//灞曠ず鎵�鏈夌敵璇疯〃
-	public int GetNextAppId();//鑾峰彇涓嬩竴涓敵璇疯〃鍗曞彿
-	//2.鍏ュ簱鐢宠琛ㄨ缁�
-	public List<StockInAppdetail> ShowAppdetail(int id);//鏄剧ず鐢宠琛ㄤ俊鎭�
-	public void Appsubmit(@Param("id")int id, //鎻掑叆涓�娆＄敵璇疯褰�
+	//1.入库申请表汇总
+	public List<StockInAppsum> ShowAppsum(int state);//展示所有申请表
+	public int GetNextAppId();//获取下一个申请表单号
+	//2.入库申请表详细
+	public List<StockInAppdetail> ShowAppdetail(int id);//显示申请表信息
+	public void Appsubmit(@Param("id")int id, //插入一次申请记录
 			@Param("applicant")String applicant,@Param("name")String name,@Param("size")String size,
 			@Param("request")String request);
-	public void Appcheck(@Param("id")int id, //鐢宠琛ㄥ鏍�
+	public void Appcheck(@Param("id")int id, //申请表审核
 			@Param("name")String name, @Param("result")String result, @Param("reason")String reason,
 			@Param("auditor")String auditor);
-	//3.鍏ュ簱璁板綍琛�
-	public List<StockInRecord> ShowRecord();//鏄剧ず鍏ュ簱璁板綍琛�
-	public List<StockInRecord> ShowReadyRecord();
-	public List<StockInRecord> ShowRecordbySample_id(@Param("sample_id")String sample_id);//鏍规嵁缂栫爜鏄剧ず鐢宠璁板綍涓叿浣撴牱鏈�
-	public int GetNextRecordId();//鑾峰彇涓嬩竴涓叆搴撹褰曞彿
-	public List<StockInAppdetail> GetNotFinishedApp();//鏄剧ず宸查�氳繃鏈櫥璁扮殑鐢宠
-	public void Recordsubmit(@Param("id")int id,//鎻愪氦涓�娆℃牱鏈褰�
+	//3.入库记录记录
+	public List<StockInRecord> ShowRecord();//显示入库记录表
+	public List<StockInRecord> ShowReadyRecord();//显示待存储的记录
+	public List<StockInRecord> ShowRecordbySample_id(@Param("sample_id")String sample_id);//根据编码显示申请记录中具体样本
+	public int GetNextRecordId();//获取下一个入库记录号
+	public List<StockInAppdetail> GetNotFinishedApp();//显示已通过未登记的申请
+	public void Recordsubmit(@Param("id")int id,//提交一次样本记录
 			@Param("application_id")int application_id,@Param("sample_quantity")int sample_quantity, 
 			@Param("sample_id") String sample_id, @Param("sample_name")String sample_name);
 }
--- src/com/bsb/service/impl/StockInServiceImpl.java
+++ src/com/bsb/service/impl/StockInServiceImpl.java
@@ -18,40 +18,40 @@ public class StockInServiceImpl implements StockInService{
 	@Autowired
 	private StockInMapper StockInMapper;
 	
-	/*1.鍏ュ簱鐢宠琛ㄦ眹鎬�*/
-	//灞曠ず鎵�鏈夌敵璇疯〃
+	/*1.入库申请表汇总*/
+	//展示所有申请表
 	@Override
 	public List<StockInAppsum> ShowAppsum(int state) {
 		return StockInMapper.ShowAppsum(state);
 	}
 	
-	//鑾峰彇涓嬩竴涓敵璇疯〃鍗曞彿
+	//获取下一个申请表单号
 	@Override
 	public int GetNextAppId() {
 		return StockInMapper.GetNextAppId();
 	}
 	
-	/*2.鍏ュ簱鐢宠琛ㄨ缁�*/
-	//鏄剧ず鐢宠琛ㄤ俊鎭�
+	/*2.入库申请表详细*/
+	//显示申请表信息
 	@Override
 	public List<StockInAppdetail> ShowAppdetail(int id) {
 		return StockInMapper.ShowAppdetail(id);
 	}
 	
-	//鎻掑叆涓�娆＄敵璇疯褰�
+	//插入一次申请记录
 	@Override
 	public void Appsubmit(int id, String applicant, String name,String size,String request) {
 		StockInMapper.Appsubmit(id, applicant, name, size, request);
 	}
 	
-	//鐢宠琛ㄥ鏍�
+	//申请表审核
 	@Override
 	public void Appcheck(int id, String name, String result, String reason,String auditor) {
 		 StockInMapper.Appcheck(id, name, result, reason, auditor);
 	}
 	
-	/*3.鍏ュ簱璁板綍琛�*/
-	//鏄剧ず鍏ュ簱璁板綍琛�
+	/*3.入库记录记录*/
+	//显示入库记录表
 	@Override
 	public List<StockInRecord> ShowRecord(){
 		return StockInMapper.ShowRecord();
@@ -62,25 +62,25 @@ public class StockInServiceImpl implements StockInService{
 		return StockInMapper.ShowReadyRecord();
 	}
 	
-	//鏍规嵁缂栫爜鏄剧ず鐢宠璁板綍涓叿浣撴牱鏈�
+	//根据编码显示申请记录中具体样本
 	@Override
 	public List<StockInRecord> ShowRecordbySample_id(String sample_id){
 		return StockInMapper.ShowRecordbySample_id(sample_id);
 	}
 	
-	//鑾峰彇涓嬩竴涓叆搴撹褰曞彿
+	//获取下一个入库记录号
 	@Override
 	public int GetNextRecordId() {
 		return StockInMapper.GetNextRecordId();
 	}
 	
-	//鏄剧ず宸查�氳繃鏈櫥璁扮殑鐢宠
+	//显示已通过未登记的申请
 	@Override
 	public List<StockInAppdetail> GetNotFinishedApp(){
 		return StockInMapper.GetNotFinishedApp();
 	}
 	
-	//鎻愪氦涓�娆℃牱鏈褰�
+	//提交一次样本记录
 	@Override
 	public void Recordsubmit(int id,int application_id,int sample_quantity, String sample_id, String sample_name) {
 		 StockInMapper.Recordsubmit(id,  application_id, sample_quantity, sample_id, sample_name);
