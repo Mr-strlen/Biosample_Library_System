7ee776a448306eae7b4445d8ee9dd74c59dcb16b
warku123 <warku123@users.noreply.github.com>
2020-07-21 20:12:55 +0800

    Update DispatchMapper.java
    编码错误更改注释

--- src/com/bsb/dao/DispatchMapper.java
+++ src/com/bsb/dao/DispatchMapper.java
@@ -12,51 +12,51 @@ import com.bsb.model.DispatchRecord;
 
 @Mapper
 public interface DispatchMapper {
-	// 1.鏄剧ず鍑哄簱鐢宠
-	// 鏄剧ず鍏ㄩ儴鍑哄簱鐢宠
+	// 1.显示出库申请
+	// 显示全部出库申请
 	public List<DispatchAppSum> findAllApp();
-	// 鏄剧ず鎸囧畾鍗曞彿鐨勫嚭搴撶敵璇�
+	// 显示指定单号的出库申请
 	public List<DispatchAppSum> findAppByOrder(String order);
-	// 鐢宠鍗曚綅
+	// 申请单位
 	public List<DispatchAppSum> findAppByApplicant(String applicant);
-	// 鏃堕棿
+	// 时间
 	public List<DispatchAppSum> findAppByTime(String time);
-	// 鐢宠缁撴灉
+	// 申请结果
 	public List<DispatchAppSum> findAppByResult(String result);
-	// 瀹℃牳鍛�
+	// 审核员
 	public List<DispatchAppSum> findAppByAuditor(String auditor);
-	// 鏇存敼鐢宠缁撴灉
+	// 更改申请结果
 	public void appCheckFinal(@Param("order")String order, @Param("result")String result, @Param("auditor")String auditor);
-	// 鑾峰彇褰撳墠璁㈠崟鏁伴噺鐢ㄤ簬璁㈠崟缂栧彿
+	// 获取当前订单数量用于订单编号
 	public Integer getApplyNum();
 	public void insertApplySum(@Param("order")String order, @Param("applicant")String applicant, @Param("time")String time, @Param("result")String result);
 	
-	// 2.鏄剧ず鍑哄簱鐢宠璇︽儏
+	// 2.显示出库申请详情
 	public void insertApplyDe(@Param("order")String order, @Param("type")String type, @Param("num")int num, @Param("condition")String condition, @Param("result")String result);
-	// 鏄剧ず鎸囧畾鍗曞彿鍑哄簱鐢宠璇︽儏
+	// 显示指定单号出库申请详情
 	public List<DispatchAppDetail> findAppDetailByOrder(String order);
-	// 鎸囧畾鍗曟樉绀哄嚭搴撲綅缃鎯�
+	// 指定单显示出库位置详情
 	public List<String> getPosByOrder(String order);
-	// 濉啓璇︽儏琛�
+	// 填写详情表
 	public void appCheck(@Param("order")String order, @Param("type")String type, @Param("result")String result, @Param("reason_pos")String reason_pos);
 	
-	// 3.鏄剧ず鍑哄簱璁板綍
-	// 鏄剧ず鍏ㄩ儴鍑哄簱璁板綍
+	// 3.显示出库记录
+	// 显示全部出库记录
 	public List<DispatchRecord> findAllRecord();
-	// 鏄剧ず鎸囧畾鍗曞彿鐨勫嚭搴撶敵璇�
+	// 显示指定单号的出库申请
 	public List<DispatchRecord> findRecordByOrder(String order);
-	// 鏃堕棿
+	// 时间
 	public List<DispatchAppSum> findRecordByTime(String time);
-	// 鍑哄簱浜哄憳
+	// 出库人员
 	public List<DispatchRecord> findRecordByManager(String manager);
-	// 璁㈠崟鐘舵��
+	// 订单状态
 	public List<DispatchRecord> findRecordByState(String state);
-	// 鑾峰彇褰撳墠璁㈠崟鏁伴噺鐢ㄤ簬璁㈠崟缂栧彿
+	// 获取当前订单数量用于订单编号
 	public Integer getRecordNum();
-	// 鑾峰彇鍑哄簱鐢宠鍗曞搴旂殑鍑哄簱鍗�
+	// 获取出库申请单对应的出库单
 	public String getAppOrderByOrder(String application_order);
-	// 鏂板涓�鏉″嚭搴撹鍗�
+	// 新增一条出库订单
 	public void insertRecord(@Param("order")String order, @Param("application_order")String application_order, @Param("state")String state);
-	// 淇敼鍑哄簱璁㈠崟鐘舵��
+	// 修改出库订单状态
 	public void updateRecord(@Param("order")String order, @Param("time")String time, @Param("manager")String manager, @Param("state")String state);
 }
