f16ae174b0a1827978424605fcc6c85d6904be39
FrankCui <FrankCui713@163.com>
2020-07-22 19:35:25 +0800

    注释

--- src/com/bsb/controller/WarehouseController.java
+++ src/com/bsb/controller/WarehouseController.java
@@ -21,21 +21,21 @@ public class WarehouseController {
     private StockInService StockInService;
 	@Autowired
     private DispatchService dispatchService;
-
+	//变动表查询界面
 	@RequestMapping("/alterquery")
     public ModelAndView AlterQuery(){
         ModelAndView mv = new ModelAndView("WarehouseQuery");
         mv.addObject("alterquery",warehouseService.FindAlter());	
         return mv;
     }
-	
+	//按照控制变中样本名字查询变动表界面
 	@RequestMapping("/alter_namequery")
     public ModelAndView Alter_NameQuery(String type){
         ModelAndView mv = new ModelAndView("Warehouse_AlterQuery");
         mv.addObject("alter_namequery",warehouseService.FindAlterByName(type));	
         return mv;
     }
-	
+	//控制表查询界面
 	@RequestMapping("/controlquery")
     public ModelAndView ControlQuery(String type, String word){
         ModelAndView mv = new ModelAndView("WarehouseQuery2");
@@ -62,7 +62,7 @@ public class WarehouseController {
         }	
         return mv;
     }
-	
+	//出入库申请样本查询界面
 	@RequestMapping("/ready")
     public ModelAndView getReady(){
         ModelAndView mv = new ModelAndView("WarehouseReady");
@@ -70,7 +70,7 @@ public class WarehouseController {
         mv.addObject("record_select",dispatchService.findAllRecord());
         return mv;
     }
-	
+	//出入库待处理样本查询界面
 	@RequestMapping("/readytohandle")
     public ModelAndView ReadytoHandle(){
         ModelAndView mv = new ModelAndView("WarehouseReady");
@@ -78,13 +78,13 @@ public class WarehouseController {
         mv.addObject("record_select",dispatchService.findRecordByState("待完成"));
         return mv;
     }
-	
+	//填入变动表界面
 	@RequestMapping("/store")
     public ModelAndView getStore(RedirectAttributes attributes){
         ModelAndView mv = new ModelAndView("WarehouseStore");
         return mv;
     }
-	
+	//变动表提交到数据库中
 	@RequestMapping("/store_submit") 
     public ModelAndView creatNewStore(String name,String alter,String reason,String operator,int num, String time,RedirectAttributes attributes){
 		ModelAndView mv = new ModelAndView("redirect:store2");
@@ -98,60 +98,60 @@ public class WarehouseController {
 		attributes.addFlashAttribute("forfun3", num);
 		return mv;
     }
-	
+	//修改仓库大小界面
 	@RequestMapping("/changewarehouse")
     public ModelAndView changeWarehouse(){
         ModelAndView mv = new ModelAndView("Warehousetotal_Alter");
         return mv;
     }
-	
+	//将修改的仓库大小提交到数据库中
 	@RequestMapping("/changewarehouse_submit")
     public ModelAndView changeWarehouse_Submit(String warehouse,int normal,int refrigerate,int freeze){
 		warehouseService.setWarehousetotal(warehouse, normal, refrigerate, freeze);
 		ModelAndView mv = new ModelAndView("redirect:areastate");	
         return mv;
     }
-	
+	//填入库存控制表界面
 	@RequestMapping("/store2")
     public ModelAndView getStore2(){
         ModelAndView mv = new ModelAndView("WarehouseStore2");
         return mv;
     }
-	
+	//库存控制表提交到数据库中
 	@RequestMapping("/store2_submit")
     public ModelAndView creatNewStore2(String name, int num, String con, String area, String time, String note, RedirectAttributes attributes){
 		warehouseService.SampleStore2(name, num, con, area, time, note);
 		ModelAndView mv = new ModelAndView("redirect:store_success");
         return mv;	
     }
-	
+	//存储成功界面
 	@RequestMapping("/store_success")
 	public ModelAndView applysuccess()
 	{
 		return new ModelAndView("WarehouseStoreSuccess");
 	}
-	
+	//按照存储区域显示库位信息界面
 	@RequestMapping("/areastate")
     public ModelAndView AreaState(){
         ModelAndView mv = new ModelAndView("WarehouseState_area");
         mv.addObject("areastate",warehouseService.FindState());	
         return mv;
     }
-	
+	//按照存储条件显示库位信息界面
 	@RequestMapping("/conditionstate")
     public ModelAndView ConditionState(){
         ModelAndView mv = new ModelAndView("WarehouseState_condition");
         mv.addObject("conditionstate",warehouseService.FindState2());	
         return mv;
     }
-	
+	//按照存储区域显示拥塞库位信息界面
 	@RequestMapping("/congestioncontrol")
     public ModelAndView CongestionControl(){
         ModelAndView mv = new ModelAndView("WarehouseCongestion_area");
         mv.addObject("areastate",warehouseService.congestioncontrol());	
         return mv;
     }
-	
+	//按照存储条件显示拥塞库位信息界面
 	@RequestMapping("/congestioncontrol2")
     public ModelAndView CongestionControl2(){
         ModelAndView mv = new ModelAndView("WarehouseCongestion_detail");
--- src/com/bsb/service/WarehouseService.java
+++ src/com/bsb/service/WarehouseService.java
@@ -8,19 +8,34 @@ import com.bsb.model.WarehouseControl;
 import com.bsb.model.WarehouseSamplestate;
 
 public interface WarehouseService {
+	//获取变动表
 	public List<WarehouseAlter> FindAlter();
+	//通过名字查询库存变动表
 	public List<WarehouseAlter> FindAlterByName(String name);
+	//获取变动编号
 	public Integer getAlterNum();
+	//插入库存变动表信息
 	public List<WarehouseAlter> SampleStore(String code, String name,String alter,String reason,String operator,int num, String time);
+	//插入库存控制表信息
 	public List<WarehouseControl> SampleStore2(String name,int num,String con,String area,String time,String note);
+	//库存控制表
 	public List<WarehouseControl> FindControl();
+	//通过名字查询库存控制表
 	public List<WarehouseControl> findControlByName(String name);
+	//通过存储条件查询库存控制表
 	public List<WarehouseControl> findControlByCondition(String condition);
+	//通过存储区域查询库存控制表
 	public List<WarehouseControl> findControlByArea(String area);
+	//通过时间查询库存控制表
 	public List<WarehouseControl> findControlByTime(String time);
+	//查询按照区域划分的库位状态表
 	public List<WarehouseState> FindState();
+	//更改仓库大小
 	public void setWarehousetotal(String warehouse,int normal,int refrigerate,int freeze);
+	//查询按照条件划分的库位状态表
 	public List<WarehouseSamplestate> FindState2();	
+	//获取处于拥塞的按照区域划分的库位状态表
 	public List<WarehouseState> congestioncontrol();
+	//获取处于拥塞的按照条件划分的库位状态表
 	public List<WarehouseSamplestate> congestioncontrol2();
 }
